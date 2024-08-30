9c16c2ec8d9ccd306e1b1f547433995156c08cdd
Syed Ukkashah <115150510+ukkashah11@users.noreply.github.com>
2024-08-30 18:32:52 +0000

    commit

--- Lab/02/Q1.cpp
+++ Lab/02/Q1.cpp
@@ -1,1 +1,25 @@
-#
+#include "iostream"
+using namespace std;
+int main()
+{
+    int **arr = new int*[3]; 
+    int Size[3];
+    int i,j,k;
+    for(i=0;i<3;i++){
+        cout<<"Row "<<i+1<< " size: ";
+        cin>>Size[i];
+        if (Size[i]!=10) Size[i] = 10; //resizing arr to 10 
+        arr[i] = new int[Size[i]];
+    }
+    for(i=0;i<3;i++){
+        for(j=0;j<Size[i];j++){ 
+            cout<<"Enter row " <<i+1<<" elements: "; 
+            cin>>*(*(arr + i)+ j);
+        }
+    }
+for (size_t i = 0; i < 3; i++)
+{
+    for (size_t j = 0; j < Size[i]; j++) cout<<"element at ["<< i<<"]["<<j<<"]: "<<arr[i][j]<<endl;
+}
+delete[] arr;
+}
---
+++ Lab/02/Q2/matrix_multiply.h
@@ -0,0 +1,11 @@
+int* matrix_mul(int** arr, int** arr2, int r1, int c1, int r2, int c2)
+{
+    int** result = new int*[r1];
+    for (int i = 0; i < r1; i++) result[i] = new int
+    
+
+   if(c1 == r2)
+   {
+
+   }
+}
