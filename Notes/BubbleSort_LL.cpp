2e7e085d3f47250ffde67d177116ad730a0c41e1
Syed Ukkashah <115150510+ukkashah11@users.noreply.github.com>
2024-09-19 14:34:27 +0500

    Update BubbleSort_LL.cpp

--- Notes/BubbleSort_LL.cpp
+++ Notes/BubbleSort_LL.cpp
@@ -1,1 +1,66 @@
-
+#include "iostream"
+using namespace std;
+class node{
+    public:
+    node* next;
+    int data;
+    node(int data):next(), data(data){}
+    friend class SLL;
+};
+class SLL{
+    public:
+    node* head;
+    SLL():head(NULL){}
+    void insertHead(const int d)
+    {
+        node* n = new node(d);
+        if(!head) head = n;
+        else{
+            n->next = head;
+            head = n;
+        }
+    }
+    void bubbleSort()
+    {
+        bool swapped;
+        do{
+            swapped = false;
+            node* curr = head;
+            while(curr && curr->next)
+            {
+                if(curr->data > curr->next->data)
+                {
+                    int temp = curr->data;
+                    curr->data = curr->next->data;
+                    curr->next->data = temp;
+                    swapped = true;
+                }
+                curr = curr->next;
+            }
+        }while(swapped);
+    }
+    void print()const{
+        node* curr =  head;
+        while(curr){ 
+            cout<<curr->data<<" ";
+            curr = curr->next;
+        }
+        cout<<endl;
+    }
+};
+int main()
+{
+    SLL l;
+    l.insertHead(2);
+    l.insertHead(5);
+    l.insertHead(1);
+    l.insertHead(0);
+    l.insertHead(7);
+    l.insertHead(4);
+    l.insertHead(9);
+    l.insertHead(6);
+    l.print();
+    l.bubbleSort();
+    l.print();
+    return 0;
+}
