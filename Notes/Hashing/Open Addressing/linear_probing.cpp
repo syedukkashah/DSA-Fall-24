b032e2a1aa29c2807bf69c53bf9ac831e50d1dd1
Syed Ukkashah <115150510+syedukkashah@users.noreply.github.com>
2024-12-22 21:49:33 +0500

    Update linear_probing.cpp

--- Notes/Hashing/Open Addressing/linear_probing.cpp
+++ Notes/Hashing/Open Addressing/linear_probing.cpp
@@ -10,6 +10,7 @@ class DataItem{
 class HashTable{
     int size, curr_size;
     vector<DataItem*> table;
+    DataItem* dummy = new DataItem(-1,-1);
     int hashCode(int key){return key%size;}
     public:
     HashTable(int s):size(s), curr_size(0)
@@ -18,21 +19,35 @@ class HashTable{
     }
     void insert(int key, int data)
     {
+        if(curr_size==size)
+        {
+            cout<<"Table full"<<endl;
+            return;
+        } 
         int idx = hashCode(key);
         DataItem* d = new DataItem(key, data);
-        while(table[idx]) idx = (idx+1)%size; //linear probing
+        while(table[idx] && table[idx]!=dummy)
+        {
+            if(table[idx]->key == key) //check for duplicates
+            {
+                table[idx]->data = data;//update data of pre existing key
+                delete d;
+                return; 
+            } 
+            idx = (idx+1)%size; //linear probing
+        } 
         table[idx] = d;
         curr_size++; //increment curr size
     }
     void deleteKey(int key)
     {
         int idx = hashCode(key);
-        while(table[idx])
+        while(table[idx] && table[idx]!=dummy)
         {
-            if(table[idx]->key == key)
+            if(table[idx]->key == key && table[idx]->key != -1)
             {
                 DataItem* temp = table[idx];
-                table[idx] = NULL;
+                table[idx] = dummy; //we will init the idx to dummy data instead of null so it acts as a placeholder when we're searching for elements
                 delete temp;
                 curr_size--; //decrement table size
                 return;
@@ -45,7 +60,7 @@ class HashTable{
         int idx = hashCode(key);
         while(table[idx])
         {
-            if(table[idx]->key == key) return table[idx];
+            if(table[idx]->key == key && table[idx]->key!=-1) return table[idx];
             idx = (idx+1)%size;
         }
         return NULL;
@@ -54,9 +69,15 @@ class HashTable{
     {
         for(DataItem* d: table)
         {
-            if(d) cout<<"("<<d->key<<","<<d->data<<") ";
+            if(d && d!=dummy) cout<<"("<<d->key<<","<<d->data<<") ";
             else cout<<" ~ ";
         }
+        cout<<endl;
+    }
+    ~HashTable()
+    {
+        for (DataItem* d : table) delete d;
+        delete dummy;
     }
 };
 int main()
@@ -71,6 +92,12 @@ int main()
     ht.insert(17, 11);
     ht.insert(13, 78);
     ht.insert(37, 97);
+    ht.display();
+    if(ht.search(4)) cout<<"key found"<<endl;
+    else cout<<"key not found"<<endl;
+    if(ht.search(44)) cout<<"key found"<<endl;
+    else cout<<"key not found"<<endl;
+    ht.deleteKey(12);
     ht.display();
     return 0;
 }
